98700374487540a69cef74452bbedd954d472bfb
Grace Kim <goooyoon@gmail.com>
2020-06-14 19:28:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+        attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
   end
 
   def self.all
